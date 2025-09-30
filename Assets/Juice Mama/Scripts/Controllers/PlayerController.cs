d3cdc79dbf996b892600032d77801e7bd76ecfe5
Moh. Yosry Zorainy <mohamedu3@gmail.com>
2025-09-30 20:51:46 +0300

    Basic Scene Layout
    - Created the basic layout of the scene with privatives and temporary materials.

---
+++ Assets/Juice Mama/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 15.0f;
+    private float horizontalInput;
+    private float verticalInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    }
+}
