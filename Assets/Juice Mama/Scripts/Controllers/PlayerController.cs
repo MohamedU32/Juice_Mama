2e28f03bdb0c496ea1e9bcfe50731c83bb37b1a0
Moh. Yosry Zorainy <mohamedu3@gmail.com>
2025-10-16 12:19:05 +0300

    Background Music changed and its volume lowered.
    - Replaced the Background music with Andrew's and lowered its volume to 0.3.

--- Assets/Juice Mama/Scripts/Controllers/PlayerController.cs
+++ Assets/Juice Mama/Scripts/Controllers/PlayerController.cs
@@ -33,6 +33,13 @@ public class PlayerController : MonoBehaviour
         if (!playerAnimator) playerAnimator = GetComponentInChildren<Animator>();
     }
 
+    private void Start()
+    {
+
+        AudioManager.Instance.PlaySound(AudioNames.BACKGROUND_MUSIC);
+        AudioManager.Instance.SetVolume(AudioNames.BACKGROUND_MUSIC, 0.3f);
+    }
+
     // Update is called once per frame
     void Update()
     {
