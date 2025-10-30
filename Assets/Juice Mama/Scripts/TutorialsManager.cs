609ad59cac62428995f990c10f8bdb7f496c33e0
Mohamed Yosry Zorainy <66830408+MohamedU32@users.noreply.github.com>
2025-10-30 16:57:27 +0300

    Tutorial Arrow (part.2)

--- Assets/Juice Mama/Scripts/TutorialsManager.cs
+++ Assets/Juice Mama/Scripts/TutorialsManager.cs
@@ -6,8 +6,8 @@ public class TutorialsManager : MonoBehaviour
     public GameObject player;
     private StorageController playerStorage;
     public PlayerData playerData;
-    public GameObject onScreenArrow;
-    private Vector3 arrowTarget = Vector3.zero;
+    public GameObject Arrow;
+    public GameObject arrowTarget;
     public float detectionRadius = 2f;
     public float currentPlayerMoney = 0;
     public Vector3 currentPlayerPosition = Vector3.zero;
@@ -42,8 +42,9 @@ public class TutorialsManager : MonoBehaviour
 
     void Start()
     {
+        //Arrow.SetActive(true);
         playerStorage = player.GetComponent<StorageController>();
-        arrowTarget = onScreenArrow.GetComponent<TutorialsArrow>().target.transform.position;
+        arrowTarget = Arrow.GetComponent<TutorialsArrow>().target;
         UIManager.Instance.ShowInstruction("Go to the Farm");
     }
 
@@ -52,66 +53,77 @@ public class TutorialsManager : MonoBehaviour
         if (tutorialStep == 1 && firstAppleTree != null && CheckProximity(firstAppleTree))
         {
             UIManager.Instance.UpdateInstructions("Unlock your first tree by tapping the yellow arrow.");
+            arrowTarget = firstAppleTree;
             tutorialStep++;
         }
 
         if (tutorialStep == 2 && appleTreeLock1 !=null && appleTreeLock1.isUnlockedByDefault)
         {
             UIManager.Instance.UpdateInstructions("Wait for the fruits to grow. Then, tap on them to collect them.");
+            arrowTarget = firstAppleTree;
             tutorialStep++;
         }
 
         if (tutorialStep == 3 && playerStorage!= null && playerStorage.GetFruitCount() > 0)
         {
             UIManager.Instance.UpdateInstructions("Go to the Kitchen.");
+            arrowTarget = firstAppleTree;
             tutorialStep++;
         }
 
         if (tutorialStep == 4 && appleJuicer != null && CheckProximity(appleJuicer))
         {
             UIManager.Instance.UpdateInstructions("Unlock your first juicer by tapping the yellow arrow.");
+            arrowTarget = appleJuicer;
             tutorialStep++;
         }
 
         if (tutorialStep == 5 && appleJuicerLock != null && appleJuicerLock.isUnlockedByDefault)
         {
             UIManager.Instance.UpdateInstructions("Tap the yellow arrow above the blender to start creating juice.");
+            arrowTarget = appleJuicer;
             tutorialStep++;
         }
 
         if (tutorialStep == 6 && playerStorage != null && playerStorage.GetFruitCount() == 0)
         {
             UIManager.Instance.UpdateInstructions("Wait for the juice to be ready. Then, tap on them to collect them.");
+            arrowTarget = appleJuicer;
             tutorialStep++;
         }
 
         if (tutorialStep == 7 && playerStorage != null && playerStorage.GetJuiceCount() > 0)
         {
             UIManager.Instance.UpdateInstructions("Go to the fridge.");
+            arrowTarget = appleJuicer;
             tutorialStep++;
         }
 
         if (tutorialStep == 8 && fridge != null && CheckProximity(fridge))
         {
             UIManager.Instance.UpdateInstructions("Put the juice in the fridge by tapping the yellow arrow.");
+            arrowTarget = fridge;
             tutorialStep++;
         }
 
         if (tutorialStep == 9 && playerStorage != null && playerStorage.GetJuiceCount() == 0)
         {
             UIManager.Instance.UpdateInstructions("Go to the apple stand area.");
+            arrowTarget = fridge;
             tutorialStep++;
         }
 
         if (tutorialStep == 10 && appleJuiceStand != null && CheckProximity(appleJuiceStand))
         {
             UIManager.Instance.UpdateInstructions("Unlock your first stand by tapping the yellow arrow.");
+            arrowTarget = appleJuiceStand;
             tutorialStep++;
         }
 
         if (tutorialStep == 11 && appleStandLock != null && appleStandLock.isUnlockedByDefault)
         {
             UIManager.Instance.UpdateInstructions("Wait for your first customer. Then, tap the yellow arrow to sell the juice.");
+            arrowTarget = appleJuiceStand;
             tutorialStep++;
             currentPlayerMoney = playerData.money;
         }
@@ -119,18 +131,21 @@ public class TutorialsManager : MonoBehaviour
         if (tutorialStep == 12 && playerData != null && playerData.money > currentPlayerMoney)
         {
             UIManager.Instance.UpdateInstructions("Go outside to unlock a new section");
+            arrowTarget = appleJuiceStand;
             tutorialStep++;
         }
 
         if (tutorialStep == 13 && firstFarmSeparator != null && CheckProximity(firstFarmSeparator))
         {
             UIManager.Instance.UpdateInstructions("Unlock the new section by tapping the yellow arrow.");
+            arrowTarget = firstFarmSeparator;
             tutorialStep++;
         }
 
         if (tutorialStep == 14 && orangeGateLock != null && orangeGateLock.isUnlockedByDefault)
         {
             UIManager.Instance.UpdateInstructions("Congratulations on Completing the Tutorials. Use the acquired knoweldege to expand your business and increase your earnings.");
+            arrowTarget = firstFarmSeparator;
             tutorialStep++;
             currentPlayerPosition = player.transform.position;
         }
@@ -139,6 +154,7 @@ public class TutorialsManager : MonoBehaviour
         {
             UIManager.Instance.UpdateInstructions("To review this tutorial click on the (?) icon.");
             tutorialStep++;
+            Arrow.SetActive(false);
             StartCoroutine(HidingInstructions());
         }
     }
